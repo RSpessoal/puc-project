d863383f623bbbf288c7d48b7785ad3b62f355f3
RSpessoal <scavonerpessoal@gmail.com>
2022-11-15 11:02:13 -0300

    add read models function

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import_os
+import_pickle
 from fastapi import FastAPI
 
+def_load_models():
+    model = pickle.load(open(os.path.join(os.getcwd(),
+                                          'models',
+                                          'model.pkl'), 'rb'))
+    scaler = pickle.load(open(os.path.join(os.getcwd(),
+                                           'models',
+                                           'scaler.pkl'), 'rb'))
+    return scaler, model
+
+
+scaler, model = load_models()
 app = FastAPI()
 
 
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import pytest
+from sklearn_ensemble import RandomForestClassifier
+from sklearn_preprocessing import StandardScaler
 from fastapi.testclient import TestClient
-from main import app
+from main import app, scaler, model
 
 client = TestClient(app)
 
@@ -8,3 +10,7 @@ client = TestClient(app)
 def test_home():
     response = client.get("/")
     assert response.json() == {"Hello": "World"}
+
+def_test_models():
+    assert_isinstance(scaler, StandardScaler)
+    assert_isinstance(model, RandomForestClassifier)
